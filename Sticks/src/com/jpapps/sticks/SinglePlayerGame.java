83c1847ad97b0fb45d5145b54e01e239d22f86c4
Paul <thesamuraipanda@gmail.com>
2013-03-11 09:12:52 -0400

    Starting work on SinglePlayerGame

--- Sticks/src/com/jpapps/sticks/SinglePlayerGame.java
+++ Sticks/src/com/jpapps/sticks/SinglePlayerGame.java
@@ -1,7 +1,11 @@
 package com.jpapps.sticks;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class SinglePlayerGame extends Activity {
 
@@ -9,6 +13,31 @@ public class SinglePlayerGame extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_single_player_game);
+	}
+	
+	protected class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+		
+		
+		
+		public GameSurfaceView(Context context) {
+			super(context);
+		}
+
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+			
+		}
+
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			
+		}
+
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			
+		}
+		
 	}
 
 }
