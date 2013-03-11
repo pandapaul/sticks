ed19a5a8c59f822a2c332f956c03d2e4b62be412
Paul <thesamuraipanda@gmail.com>
2013-03-11 00:55:15 -0400

    First commit of Sticks. Cowabunga.

---
+++ Sticks/src/com/jpapps/sticks/MainMenu.java
@@ -0,0 +1,32 @@
+package com.jpapps.sticks;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+	
+	public void tap(View view) {
+		switch(view.getId()) {
+		case R.id.MainMenuSinglePlayer:
+			Intent i = new Intent(this, com.jpapps.sticks.SinglePlayerGame.class);
+			startActivity(i);
+		}
+	}
+
+}
---
+++ Sticks/src/com/jpapps/sticks/SinglePlayerGame.java
@@ -0,0 +1,14 @@
+package com.jpapps.sticks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SinglePlayerGame extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_single_player_game);
+	}
+
+}
